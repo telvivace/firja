8f83056f57965566cb7c74aa39ff214cc8fc3b47
andrej <andrej.vencelj.007@gmail.com>
2024-09-25 14:47:20 +0200

    implemented new() and is_root() for treeNode

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use simple;
 
+const PLANE: (f64, f64) = (100f64, 100f64);
 
 enum Object<'tree> {
     Free(&'tree object),
     Hit(&'tree object, Vec<object>)
 }
 struct object {
-    x : f64,
-    y : f64,
-    m : u32,
-    v : (f32, f32),
-    s : f32,
+    x: f64,
+    y: f64,
+    m: u32,
+    v: (f32, f32),
+    s: f32,
 }
 enum NodeReference<'tree> {
     Node(&'tree TreeNode<'tree>),
@@ -26,11 +27,26 @@ enum TreeNode<'tree> {
     Right(treeNode<'tree>)
 }
 struct treeNode<'tree> {
-    split : TreeSplit,
-    left  : NodeReference<'tree>,
-    right : NodeReference<'tree>,
+    split: TreeSplit,
+    left: NodeReference<'tree>,
+    right: NodeReference<'tree>,
     parent: NodeReference<'tree>,
+}
 
+impl<'tree> treeNode<'tree> {
+    fn new() -> Self {
+        treeNode {
+            split: TreeSplit::X(PLANE.0 / 2f64),
+            left: NodeReference::Stub,
+            right: NodeReference::Stub,
+            parent: NodeReference::Stub,
+        }
+    }
+
+    //The root is unique in that it has Stub as a parent
+    fn is_root(&self) -> bool {
+        matches!(self.parent, NodeReference::Stub)
+    }
 }
 
 fn main() {
