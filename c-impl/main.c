0d0be6db63915bb6b6f287071184198a4686d00d
Klemen Javoršek <klemen.javorsek@guest.arnes.si>
2024-11-23 13:29:58 +0100

    main now has an option to specify number of objects with a command line argument

--- c-impl/main.c
+++ c-impl/main.c
@@ -2,7 +2,10 @@
 #include <tgmath.h>
 #include <unistd.h>
 #define ever ;;
-int main(){
+int main(int argc, char* argv[static 1]){
+    int numObjects = 40;
+    if(argc > 1)
+        numObjects =  atoi(argv[1]); 
     printf("start of main\n");
     globalInformation* globalInfo = malloc(sizeof(globalInformation));
     globalInfo->bufferCount = 3;
@@ -10,13 +13,13 @@ int main(){
     printf("init tree\n");
     globalInfo->tree = tree_initTree();
     printf("start loop\n");
-    for(unsigned i = 0; i < 40; i++){
+    for(unsigned i = 0; i < numObjects; i++){
         fprintf(stderr, "iteration %d\n", i);
         tree_insertObject(globalInfo->tree, &(object){
             .m = 5, 
             .s = 5, 
             .x = 100/40*i, 
-            .y = sin(100/40*i), 
+            .y = 100*sin(100/40*i), 
             .v = (speed){
                 .x = i/100, 
                 .y = -i/100
@@ -24,6 +27,9 @@ int main(){
         });
         globalInfo->objectCount++;
     }
+    tree_printTree(globalInfo->tree);
+    printf("Buffer count: %d\n", globalInfo->tree->bufCount);
+    return 0;
     unsigned cycles = 0;
     struct timespec stoptime, starttime;
     timespec_get(&starttime, TIME_UTC);
@@ -37,6 +43,6 @@ int main(){
     timespec_get(&stoptime, TIME_UTC);
     unsigned long deltatime = (stoptime.tv_sec  - starttime.tv_sec)  * 1000000
                        + (stoptime.tv_nsec - starttime.tv_nsec) / 1000 ;
-    printf("Average FPS: %f", (double)cycles/((double)deltatime / 1000000));
-
+    printf("Average FPS: %f\n", (double)cycles/((double)deltatime / 1000000));
+    tree_printTree(globalInfo->tree);
 }
