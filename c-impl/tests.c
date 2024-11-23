4fd08009da8f01a05bb5945edadd370e02c2ebcd
Klemen Javoršek <klemen.javorsek@guest.arnes.si>
2024-11-23 13:30:20 +0100

    some tests WIP

--- c-impl/tests.c
+++ c-impl/tests.c
@@ -14,11 +14,25 @@ int test_tree_initTree(){
     tree_deleteTree(ret);
     printf("still alive and kicking\n");
     return 0;
+
+
+}
+int test_countBits(){
+    printf("1: %d\n", countSetBitsUL(1));
+    printf("7: %d\n", countSetBitsUL(7));
+    printf("8: %d\n", countSetBitsUL(8));
+    printf("10:%d\n", countSetBitsUL(10));
+}
+int test_printTree(){
+    tree_allocPool* pool = tree_allocInitPool(2000);
+    
 }
 int main(void){
     test_tree_allocInitDestroy();
     printf("PASS:   ALLOC INIT DESTROY\n");
     test_tree_initTree();
     printf("PASS:   TREE_INITTREE\n");
+    test_countBits();
     return 0;
 }
+
