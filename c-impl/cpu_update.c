14eedeaefe1fd368486dd11cd62f90c43904d171
Klemen Javoršek <klemen.javorsek@guest.arnes.si>
2025-01-03 12:33:00 +0100

    add null buffer handling, add missing returns

--- c-impl/cpu_update.c
+++ c-impl/cpu_update.c
@@ -5,12 +5,23 @@
 #include "settings.h"
 
 int vector_update_aux(treeNode* node){
+    printf("At node: %p\n", node);
     if(!node->buf){
+        printf("buffer is null. Left: %p, Right: %p\n", node->left, node->right);
         vector_update_aux(node->left);
         vector_update_aux(node->right);
+        printf("UP ( %p -> %p)\n", node, node->up);
+        return 0;
     }
+    printf("buffer is valid: %p\n", node->buf);
+    printf("\nchecking no. ");
     for(unsigned i = 0; i < OBJBUFSIZE; i++){
+        printf("%d..", i);
         object* obj = node->buf + i;
+        if(!obj){
+            printf("OBJECT NUMBER %d IS NULL!\n", i);
+            continue;
+        }
         if(obj->hit) {
             speed v_cm = {
                 .x = (obj->m * obj->v.x + obj->hit->m * obj->hit->v.x)/(obj->m + obj->hit->m),
@@ -32,20 +43,37 @@ int vector_update_aux(treeNode* node){
         if(obj->x > RIGHTBORDER || obj->x < LEFTBORDER) obj->v.x *= -1;
         if(obj->y > TOPBORDER || obj->y < BOTTOMBORDER) obj->v.x *= -1;
     }
+    putc('\n', stdout);
+    putc('\n', stdout);
     return 0;
 }
 int vector_update(objTree* tree){
+    printf("Vector update.\n");
     return vector_update_aux(tree->root);
 }
 
 int scalar_update_aux(treeNode* node){
+    if(!node->buf){
+        printf("buffer is null. Left: %p, Right: %p\n", node->left, node->right);
+        printf("going left\n");
+        scalar_update_aux(node->left);
+        printf("going right\n");
+        scalar_update_aux(node->right);
+        printf("UP ( %p -> %p)\n", node, node->up);
+        return 0;
+    }
+    printf("\nchecking no. ");
     for(unsigned i = 0; i < OBJBUFSIZE; i++){
-        object* obj = node->buf + i;
+        printf("%d..", i);
+        object* obj = node->buf + i;    
         obj->x += obj->v.x;
         obj->y += obj->v.y;
     }
+    printf("\n\n");
+    printf("UP ( %p -> %p)\n", node, node->up);
     return 0;
 }
 int scalar_update(objTree* tree){
+    printf("Scalar update.\n");
     return scalar_update_aux(tree->root);
 }
