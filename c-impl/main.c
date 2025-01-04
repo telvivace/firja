78c2931937768d2dda5c399342fb7a22f08d0156
Klemen Javoršek <klemen.javorsek@guest.arnes.si>
2025-01-04 16:52:56 +0100

    Implement visualisation using SDL2
    This is a temporary addition, because the complementary project
    for graphics has been put on hold for a while. It will eventually
    be replaced by the aforementioned project's code.

--- c-impl/main.c
+++ c-impl/main.c
@@ -2,6 +2,8 @@
 #include "hit_detect.h"
 #include "cpu_update.h"
 #include "treeutils.h"
+#include "settings.h"
+#include <SDL2/SDL.h>
 #include <stdio.h>
 #include <time.h>
 #include <tgmath.h>
@@ -15,6 +17,65 @@ void printFramesAtSegfault(int signal){
     printf("Frames rendered: %ld\n", frames);
     printf("SEGMENTATION FAULT\n");
     exit(EXIT_FAILURE);
+}
+
+// Function to draw a circle using the Midpoint Circle Algorithm
+void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+    int x = radius;
+    int y = 0;
+    int radiusError = 1 - x;
+
+    while (x >= y) {
+        // Draw symmetric points in all eight octants
+        SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
+        SDL_RenderDrawPoint(renderer, centerX - x, centerY + y);
+        SDL_RenderDrawPoint(renderer, centerX + x, centerY - y);
+        SDL_RenderDrawPoint(renderer, centerX - x, centerY - y);
+        SDL_RenderDrawPoint(renderer, centerX + y, centerY + x);
+        SDL_RenderDrawPoint(renderer, centerX - y, centerY + x);
+        SDL_RenderDrawPoint(renderer, centerX + y, centerY - x);
+        SDL_RenderDrawPoint(renderer, centerX - y, centerY - x);
+
+        y++;
+        if (radiusError < 0) {
+            radiusError += 2 * y + 1;
+        } else {
+            x--;
+            radiusError += 2 * (y - x) + 1;
+        }
+    }
+}
+void renderObjects_rec(treeNode* node, SDL_Renderer* renderer){
+    if(!node->buf){
+        printf("buffer is null. Left: %p, Right: %p\n", node->left, node->right);
+        printf("going left\n");
+        renderObjects_rec(node->left, renderer);
+        printf("going right\n");
+        renderObjects_rec(node->right, renderer);
+        printf("UP ( %p -> %p)\n", node, node->up);
+        return;
+    }
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    printf("node rect at x: %lf -- %lf  y : %lf -- %lf\n", node->bindrect.lowlow.x, node->bindrect.highhigh.x, node->bindrect.lowlow.y, node->bindrect.highhigh.y);
+    SDL_Rect rect = {
+        .x = node->bindrect.lowlow.x,
+        .y = node->bindrect.lowlow.y,
+        .w = node->bindrect.highhigh.x - node->bindrect.lowlow.x,
+        .h = node->bindrect.highhigh.y - node->bindrect.lowlow.y,
+    };
+    SDL_RenderDrawRect(renderer, &rect);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    
+    printf("\ndrawing no. ");
+    for(unsigned i = 0; i < OBJBUFSIZE; i++){
+        if(node->buf[i].s){
+            printf("draw at x:%lf y:%lf s:%f  ", node->buf[i].x, node->buf[i].y, node->buf[i].s);
+            drawCircle(renderer, node->buf[i].x + 100, node->buf[i].y + 200, node->buf[i].s);
+        }
+    }
+    printf("\n\n");
+    printf("UP ( %p -> %p)\n", node, node->up);
+    return;
 }
 int main(int argc, char* argv[static 1]){
     signal(SIGSEGV, printFramesAtSegfault);
@@ -30,31 +91,82 @@ int main(int argc, char* argv[static 1]){
     printf("start loop\n");
     for(unsigned i = 0; i < numObjects; i++){
         fprintf(stderr, "iteration %d\n", i);
+        printf("inserting object at x: %d y: %lf v: x: %lf y: %lf", 100/40*i, 100*sin(100/40*i), (float)i/100, -(float)i/100);
         tree_insertObject(globalInfo->tree, &(object){
             .m = 5, 
             .s = 5, 
-            .x = 100/40*i, 
-            .y = 100*sin(100/40*i), 
+            .x = rand() % (RIGHTBORDER - LEFTBORDER - 5),
+            .y = rand() % (TOPBORDER - BOTTOMBORDER- 5),
             .v = (speed){
-                .x = i/100, 
-                .y = -i/100
+                .x = ((rand() % 10) - 2) / 2.0f,
+                .y = ((rand() % 10) - 2) / 2.0f,
             }
         });
         globalInfo->objectCount++;
     }
     tree_printTree(globalInfo->tree);
     printf("Buffer count: %d\n", globalInfo->tree->bufCount);
+    
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("2D Canvas", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    unsigned running = 1;
+    SDL_Event e;
+
     unsigned cycles = 0;
     struct timespec stoptime, starttime;
     timespec_get(&starttime, TIME_UTC);
-    for(ever){
-        frames++;
+    while(running){
         hit_flagObjects(globalInfo->tree);
         vector_update(globalInfo->tree);
         scalar_update(globalInfo->tree);
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+        // Clear screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        printf("Drawing.\n");
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        renderObjects_rec(globalInfo->tree->root, renderer);
+
+
+        // Update and draw sprites
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        printf("presenting frame\n");
+        // Present rendered frame
+        SDL_RenderPresent(renderer);
+        // Delay to cap frame rate
+        SDL_Delay(12); // ~60 FPS
         cycles++;
-        if(cycles > 1000) break;
+        frames++;
     }
+            
+
+    // Clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
     timespec_get(&stoptime, TIME_UTC);
     unsigned long deltatime = (stoptime.tv_sec  - starttime.tv_sec)  * 1000000
                        + (stoptime.tv_nsec - starttime.tv_nsec) / 1000 ;
