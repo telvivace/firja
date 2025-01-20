905252b25f85d9285dca681ceae67f23255e56ce
Klemen Javoršek <klemen.javorsek@guest.arnes.si>
2025-01-20 20:47:09 +0100

    add an object printing function

--- c-impl/treeutils.h
+++ c-impl/treeutils.h
@@ -1,6 +1,15 @@
+#ifndef TREEUTILS_H
+#define TREEUTILS_H
 #include "objtree.h"
+#include <stdio.h>
 int tree_printTree(objTree* tree);
 
 int tree_printTreeBoxes(objTree* tree);
 
 unsigned countSetBitsUL(unsigned long N);
+
+static inline int tree_printObject(object* obj){
+    return printf("id: %ld\n x: %lf  y: %lf\n s: %lf\n",
+    obj->id, obj->x, obj->y, obj->s);
+}
+#endif
